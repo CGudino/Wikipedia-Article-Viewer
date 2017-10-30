9b7a3485a3c96f16e3bb4385c8dcf6b6020e94db
CGudino <CGudino1359@gmail.com>
2017-10-30 16:08:13 -0700

    Wiki viewer

---
+++ js/index.js
@@ -0,0 +1,27 @@
+$("#input-field").keyup(function(event){
+    if(event.keyCode == 13){
+        $("#search-button").click();
+    }
+});
+
+var search = $('#search-button');
+
+function goSearch() {
+  var input = $('#input-field').val();
+  var fullUrl = 'https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=' + input + '&format=json&callback=wikiCallback';
+  
+  $.ajax({
+  url: fullUrl,
+  dataType: 'jsonp',
+  success: function(data) {
+    var newData = data.query.search;
+    
+    for (var i = 0; i < newData.length; i++) {
+      var result = $('#result-list');
+      var wikiLink = 'https://en.wikipedia.org/wiki/' + newData[i].title;
+      
+      result.append('<div class="wiki-results"><a href=' + wikiLink + ' target="_blank" class="results">' + newData[i].title + '<br/><p class="article-desc">' + newData[i].snippet + '</p></a></div>');
+    }
+  }
+});
+}
