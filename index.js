98cea5e547c9436f9b4391c4c54f1711e37c307a
CGudino <CGudino1359@gmail.com>
2017-12-12 16:03:59 -0800

    Messing with the CSS a little

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var search = $('#search-button');
 
 function goSearch() {
   var input = $('#input-field').val();
-  var fullUrl = 'https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=' + input + '&format=json&callback=wikiCallback';
+  var fullUrl = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${input}&format=json&callback=wikiCallback`;
   
   // Resets result list when searching for something new
   $('#result-list').html('');
@@ -21,9 +21,9 @@ function goSearch() {
     
     for (var i = 0; i < newData.length; i++) {
       var result = $('#result-list');
-      var wikiLink = 'https://en.wikipedia.org/wiki/' + newData[i].title;
+      var wikiLink = `https://en.wikipedia.org/wiki/${newData[i].title}`;
       
-      result.append('<div class="wiki-results"><a href=' + wikiLink + ' target="_blank" class="results">' + newData[i].title + '<br/><p class="article-desc">' + newData[i].snippet + '</p></a></div>');
+      result.append(`<div class="wiki-results"><a href=${wikiLink} target="_blank" class="results">${newData[i].title}<br/><p class="article-desc">${newData[i].snippet}</p></a></div>`);
     }
   }
 });
